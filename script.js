1e0abf48b25c4715f09b138c64f49ff5afbb11fc
Lauramazur <lauramazur38@gmail.com>
2025-10-27 18:13:34 +0200

    Am completat pagina Acasa si Produse

---
+++ script.js
@@ -0,0 +1,30 @@
+window.addEventListener('load', () => {
+  const carousel = document.querySelector('.carousel-images');
+  const images = document.querySelectorAll('.carousel-images img');
+  const prevBtn = document.querySelector('.prev');
+  const nextBtn = document.querySelector('.next');
+  let counter = 0;
+  let size = images[0].clientWidth;
+  function updateCarousel() {
+    carousel.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  }
+  window.addEventListener('resize', () => {
+    size = images[0].clientWidth;
+    updateCarousel();
+  });
+  nextBtn.addEventListener('click', () => {
+    counter++;
+    if (counter >= images.length) counter = 0;
+    updateCarousel();
+  });
+  prevBtn.addEventListener('click', () => {
+    counter--;
+    if (counter < 0) counter = images.length - 1;
+    updateCarousel();
+  });
+  setInterval(() => {
+    counter++;
+    if (counter >= images.length) counter = 0;
+    updateCarousel();
+  }, 4000);
+});
